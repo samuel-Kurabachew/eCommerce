cfc0492dd1bac4738a2a6b0c792a1a271d39b295
samuel <samkura47@gmail.com>
2019-04-02 13:09:57 +0300

    main and login activity

--- app/src/main/java/com/sam/ecommerce/MainActivity.java
+++ app/src/main/java/com/sam/ecommerce/MainActivity.java
@@ -1,13 +1,29 @@
 package com.sam.ecommerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = findViewById(R.id.main_join_now_button);
+        loginButton = findViewById(R.id.main_login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
